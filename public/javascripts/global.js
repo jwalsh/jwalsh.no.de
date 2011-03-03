eb90efaae650384f2de10ba019fa90f1ae719acf
Jason Walsh <j@wal.sh>
2011-03-03 07:42:58 -0800

    Adding permissions for domain publishing

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,11 +1,14 @@
 console.log('app.js');
 
 window.fbAsyncInit = function() {
-  FB.init({appId: '', status: true, cookie: true,
+  FB.init({appId: document.getElementById('fb-app-id').content, 
+           status: true, 
+           cookie: true,
            xfbml: true});
 };
 
 (function() {
+
   var e = document.createElement('script'); e.async = true;
   e.src = document.location.protocol +
     '//connect.facebook.net/en_US/all.js';
